15cacba5583af83c4911749ffe3f99a5b1681c89
Kate Secor <aiglet@gmail.com>
2014-01-29 12:58:11 -0800

    v2 start
    The fresh shiny new rails project with no work done to it at all.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twdb::Application.config.secret_token = 'fc8c8ef36d2039fb7cd4e30cc4d56c3bcd588737524169794dccd85673ed0b4b689de15010ca83768d3b730e98f1ac8a910e42d461675093d1ec740f5c88ddd2'
